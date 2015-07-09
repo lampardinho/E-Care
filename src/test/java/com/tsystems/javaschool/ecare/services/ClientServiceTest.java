70fe0cc492d78f4e78392934a3e411ef29944bde
kolia <1lampard@mail.ru>
2015-07-09 18:01:39 +0300

    add tests and logging

---
+++ src/test/java/com/tsystems/javaschool/ecare/services/ClientServiceTest.java
@@ -0,0 +1,67 @@
+package com.tsystems.javaschool.ecare.services;
+
+import static org.junit.Assert.*;
+
+public class ClientServiceTest
+{
+
+    @org.junit.Before
+    public void setUp() throws Exception
+    {
+
+    }
+
+    @org.junit.Test
+    public void testSaveOrUpdateClient() throws Exception
+    {
+
+    }
+
+    @org.junit.Test
+    public void testLoadClient() throws Exception
+    {
+
+    }
+
+    @org.junit.Test
+    public void testFindClient() throws Exception
+    {
+
+    }
+
+    @org.junit.Test
+    public void testFindClientByNumber() throws Exception
+    {
+
+    }
+
+    @org.junit.Test
+    public void testDeleteClient() throws Exception
+    {
+
+    }
+
+    @org.junit.Test
+    public void testGetAllClients() throws Exception
+    {
+
+    }
+
+    @org.junit.Test
+    public void testDeleteAllClients() throws Exception
+    {
+
+    }
+
+    @org.junit.Test
+    public void testGetNumberOfClients() throws Exception
+    {
+
+    }
+
+    @org.junit.Test
+    public void testExistLogin() throws Exception
+    {
+
+    }
+}
---
+++ src/test/java/com/tsystems/javaschool/ecare/services/TariffServiceTest.java
@@ -0,0 +1,45 @@
+package com.tsystems.javaschool.ecare.services;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TariffServiceTest
+{
+
+    @Test
+    public void testSaveOrUpdateTariff() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testLoadTariff() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testDeleteTariff() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testGetAllTariffs() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testDeleteAllTariffs() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testGetNumberOfTariffs() throws Exception
+    {
+
+    }
+}
